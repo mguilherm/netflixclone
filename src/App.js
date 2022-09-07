cd21c52b7c574a3041475cab15b3913f51141741
Marcos Guilherme Silva <marcosguilherme.silva@outlook.com>
2022-09-07 10:43:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => {
+  const [movieList, setMovieList] = useState([])
+  useEffect(() => {
+    const loadAll = async () =>{
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+    loadAll();
 
-export default App;
+  }, [])
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div>
+            <MovieRow 
+              key= {key}
+              title = {item.title}
+              items={item.items}
+            />
+          </div>
+        ))}
+      </section>
+      
+    </div>
+  )
+}
